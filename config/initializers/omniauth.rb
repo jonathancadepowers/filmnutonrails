c566a69f0329fd90afeecfecb9b8e66899821415
Jonathan Powers <jonathan.powers@onit.com>
2020-06-27 15:34:45 -0500

    Fixes Spotify authentication bug.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -5,5 +5,5 @@ Rails.application.config.to_prepare do
 end
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :spotify, "59966b30b87943ce99dd736880815410", "b06aab6c2a7f489f8aa256a149791f18", scope: 'user-read-recently-played user-read-email'
+  provider :spotify, Rails.application.credentials.spotify[:client_id], Rails.application.credentials.spotify[:client_secret], scope: 'user-read-recently-played user-read-email'
 end
