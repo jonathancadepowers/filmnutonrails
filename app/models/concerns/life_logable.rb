a57ecb4aee96d8818166b970b6bd7f8e52933d30
Jonathan Powers <jonathan.powers@onit.com>
2020-06-28 17:16:46 -0500

    Continues to address time zone conversion for LifeLog.

--- app/models/concerns/life_logable.rb
+++ app/models/concerns/life_logable.rb
@@ -3,7 +3,14 @@
 module LifeLogable
   extend ActiveSupport::Concern
 
-  def self.get_day_boundary(day, boundary)
-    Date.strptime(day, "%Y%m%d").public_send(boundary + "_of_day")
+  # Ensures that an Apples to Apples query is executed.
+  # .change converts "day" to the correct context: the app's default time zone.
+  # .in_time_zone then converts "day" to UTC, since life logs are stored in UTC.
+  # Example: "2020-01-01" => 2020-01-01 06:00:00 UTC
+  def self.local_day_to_utc_boundary(day, boundary, local_offset)
+    Date.strptime(day, "%Y%m%d")
+        .public_send(boundary + "_of_day")
+        .change(offset: local_offset)
+        .in_time_zone(Time.zone)
   end
 end
--- app/models/life_log.rb
+++ app/models/life_log.rb
@@ -15,6 +15,7 @@ class LifeLog < ApplicationRecord
             :related_object_type,
             presence: true
 
+  # Converts life logs to app's default time zone before grouping them.
   def self.life_logs_set(count)
     local_time_zone = ApplicationController.helpers.app_time_zone
     LifeLog.all
@@ -26,16 +27,13 @@ class LifeLog < ApplicationRecord
            .reverse_each
   end
 
+  # Converts life logs to the app's default time zone before grouping them.
   def self.life_logs_set_single_day(day)
     local_time_zone = ApplicationController.helpers.app_time_zone
     local_offset = ApplicationController.helpers.app_time_zone_offset
-    day_start = LifeLogable.get_day_boundary(day, "beginning")
-                           .change(offset: local_offset)
-                           .in_time_zone(Time.zone)
-    day_end = LifeLogable.get_day_boundary(day, "end")
-                         .change(offset: local_offset)
-                         .in_time_zone(Time.zone)
-    LifeLog.where(display_timestamp: day_start..day_end)
+    day_beg = LifeLogable.local_to_utc_boundary(day, "beginning", local_offset)
+    day_end = LifeLogable.local_to_utc_boundary(day, "end", local_offset)
+    LifeLog.where(display_timestamp: day_beg..day_end)
            .order("display_timestamp DESC")
            .group_by_day(time_zone: local_time_zone, &:display_timestamp)
            .group_by_month(time_zone: local_time_zone) { |d| d[0] }
