d56862fdfd7d1448eae4f7b976dfd959f671899d
Jonathan Powers <jonathan.powers@onit.com>
2020-06-28 17:18:43 -0500

    Continues to address time zone conversion for LifeLog.

--- app/models/concerns/life_logable.rb
+++ app/models/concerns/life_logable.rb
@@ -7,7 +7,7 @@ module LifeLogable
   # .change converts "day" to the correct context: the app's default time zone.
   # .in_time_zone then converts "day" to UTC, since life logs are stored in UTC.
   # Example: "2020-01-01" => 2020-01-01 06:00:00 UTC
-  def self.local_day_to_utc_boundary(day, boundary, local_offset)
+  def self.local_to_utc_boundary(day, boundary, local_offset)
     Date.strptime(day, "%Y%m%d")
         .public_send(boundary + "_of_day")
         .change(offset: local_offset)
